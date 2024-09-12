5ffbd5652dfac4bf92a819e4edbd6b458515be0f
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-11 19:44:11 -0500

    Auto push

--- variables.tf
+++ variables.tf
@@ -2,5 +2,7 @@ variable "instance_type" {}
 variable "name" {}
 variable "env" {}
 variable "port_no" {}
-variable "pwd" {}
+variable "pwd" {
+    default = "DevOps321"
+}
 #variable "vault_token" {}
