2c2417f8d40828beb7a34e603c794481cc1acd49
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-28 22:14:35 -0500

    Auto push

--- sg.tf
+++ sg.tf
@@ -45,14 +45,3 @@ resource "aws_security_group_rule" "nginx_exporters" {
   cidr_blocks       = var.prometheus_node
   security_group_id = aws_security_group.main.id
 }
-
-# This rule is only specific to frontend 
-resource "aws_security_group_rule" "grok_exporters" {
-  count             = var.name == "frontend" ? 1 : 0
-  type              = "ingress"
-  from_port         = 9144
-  to_port           = 9144
-  protocol          = "tcp"
-  cidr_blocks       = var.prometheus_node
-  security_group_id = aws_security_group.main.id
-}
--- variables.tf
+++ variables.tf
@@ -5,3 +5,6 @@ variable "port_no" {}
 variable "ssh_pwd" {}
 variable "vault_token" {}
 variable "prometheus_node" {}
+variable "iam_role" {
+  default = "b58-iam-admin"
+}
