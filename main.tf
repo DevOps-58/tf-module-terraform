9e555b6de887f12155651be358f04ad4cbdca851
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-30 16:56:49 -0500

    Auto push

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 resource "aws_instance" "main" {
   ami                    = data.aws_ami.main.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main.id]
+  vpc_security_group_ids = [aws_security_group.main.id] 
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
+  }
 
   tags = {
     Name = "${var.name}-${var.env}"
